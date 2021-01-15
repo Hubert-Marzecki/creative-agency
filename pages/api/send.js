5af0d1ff4972aaf98345b12a6b36a51347700027
Hubert <hubert.marzecki@gmail.com>
2021-01-15 22:37:02 +0100

    adding email sender

---
+++ pages/api/send.js
@@ -0,0 +1,23 @@
+const sgMail = require("@sendgrid/mail");
+
+export default async function (req, res) {
+    sgMail.setApiKey(process.env.SENGRIND_API_KEY_2);
+    console.error(process.env.SENGRIND_API_KEY_2);
+    const { email, message } = req.body;
+
+    const content = {
+        to: "hubert.marzecki@gmail.com",
+        from: "hubert@marzecki.pl",
+        subject: `New Message From - ${email}`,
+        text: message,
+        html: `<p>${message}</p>`,
+    };
+
+    try {
+        await sgMail.send(content);
+        res.status(200).send("Wiadomość wysłana!");
+    } catch (error) {
+        console.log("ERROR", JSON.stringify(error.response.body.errors));
+        res.status(400).send("Nie można wysłać wiadomości");
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ import holo from '../public/holo/main.png'
 import ibwp from '../public/ibwp/main.png'
 import AOS from "aos";
 import "aos/dist/aos.css";
-
+import { v4 as uuidv4 } from 'uuid';
 
 //  TODO - add IBWP Section
 //  TODO - add ProTip Section
@@ -103,7 +103,7 @@ export default function Home(props: any): JSX.Element {
     return (
         <>
           {elements.slice(0, state.itemLimit).map((item) => (
-              <Link href={`${item.link}`} key={item.link}
+              <Link href={`${item.link}`} key={uuidv4()}
               >
                 <div
                     className="proj__tile mx-6 sm:w-4/12  mb-10 px-4 transition duration-300 ease-in-out  transform hover:-translate-y-1 hover:scale-105 cursor-pointer hover:text-yellow-500">
@@ -132,7 +132,7 @@ export default function Home(props: any): JSX.Element {
     return (
         <>
           {elements.slice(0, state.itemLimit).map((item) => (
-              <Link href={`${item.link}`} key={item.link}
+              <Link href={`${item.link}`} key={uuidv4()}
               >
                 <div
 
@@ -159,6 +159,70 @@ export default function Home(props: any): JSX.Element {
         </>
     );
   }
+
+
+  const [formStatus, setFormStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null },
+  });
+
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleResponse = (status, msg) => {
+    if (status === 200) {
+      setFormStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg: msg },
+      });
+      setInputs({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } else {
+      setFormStatus(s => { return (
+          {...s,
+            info: { error: true, msg: msg },
+          }
+      )});
+    }
+  };
+
+  const handleOnChange = (e) => {
+    e.persist();
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+    setFormStatus({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null },
+    });
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    setFormStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+    const res = await fetch("/api/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputs),
+    });
+    const text = await res.text();
+    handleResponse(res.status, text);
+  };
+
+
+
   return (
     <>
       <Header bgColor={state.bgColor}/>
@@ -271,7 +335,86 @@ export default function Home(props: any): JSX.Element {
           title=" KONTAKT "
           styleVaribles={"margin-top: 100px; color:white; padding-bottom: 0px"}
         />
-        <EmailForm />
+      {/*EMAIL*/}
+
+
+        <div className="pb-40">
+          <h1 className="font-bold text-5xl text-black text-center  ">
+            Kontakt
+          </h1>
+          <section className={`flex flex-wrap  px-10 container mx-auto justify-center`}>
+
+
+            <div className="w-screen sm:w-1/2 sm:pr-20 pb-20 sm:pl-20 ">
+              <form
+                  className=" flex flex-row justify-center flex-wrap w-full mt-10"
+                  onSubmit={handleOnSubmit}
+              >
+                <div className="flex flex-col mr-auto mt-4 w-full ">
+                  <input
+                      id="name"
+                      className="px-2  border-b  w-full py-3 rounded  text-gray-700"
+                      type="text"
+                      onChange={handleOnChange}
+                      required
+                      value={inputs.name}
+                  />
+                  <label className="label mt-2 text-white " htmlFor="name">
+                    Imię
+                  </label>
+                </div>
+                <div className="flex flex-col mr-auto mt-4 w-full ">
+                  <input
+                      id="email"
+                      className="px-2 border-b  w-full py-3 rounded  text-gray-700"
+                      type="email"
+                      onChange={handleOnChange}
+                      required
+                      value={inputs.email}
+                  />
+                  <label className="label mt-2 text-white " htmlFor="email">
+                    E-mail
+                  </label>
+                </div>
+                <div className="w-full flex flex-col mt-10">
+                <textarea
+                    className=" text-gray-700 w-full border rounded  p-2 h-40 focus:outline-none focus:border-gray-500"
+                    id="message"
+                    onChange={handleOnChange}
+                    value={inputs.message}
+                    required
+                />
+                  <label className="mt-2 text-white " htmlFor="message">
+                    Wiadomość
+                  </label>
+                </div>
+                <div className="">
+                  <button
+                      className=" mr-1 bg-yellow-600 hover:bg-yellow-500 mt-8 py-3 px-8 text-lg rounded-full font-bold uppercase text-white tracking-widest"
+                      type="submit"
+                      value="Wyślij"
+                      disabled={formStatus.submitting}
+                  >
+                    {" "}
+                    {!formStatus.submitting
+                        ? !formStatus.submitted
+                            ? "Wyślij"
+                            : "Wysłano"
+                        : "Wysyłanie..."}{" "}
+                  </button>
+                </div>
+              </form>
+              {formStatus.info.error && (
+                  <div className="error">Error: {formStatus.info.msg}</div>
+              )}
+              {!formStatus.info.error && formStatus.info.msg && (
+                  <div className="success">{formStatus.info.msg}</div>
+              )}
+
+            </div>
+          </section>
+        </div>
+
       </StyledPage>
     </>
   );
